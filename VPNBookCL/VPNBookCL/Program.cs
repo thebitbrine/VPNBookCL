ab24e9f18c5219a559e1cbc32894932ac4cb3fc7
TheBitBrine <blacknomex08@gmail.com>
2017-06-26 02:57:01 +0400

    Add files via upload

---
+++ VPNBookCL/VPNBookCL/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+
+namespace VPNBookCL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(GetPass());
+        }
+
+        public static string GetPass()
+        {
+            string htmlCode;
+            using (WebClient client = new WebClient())
+            {
+                htmlCode = client.DownloadString("https://www.vpnbook.com/#pptpvpn");
+            }
+            return getBetween(htmlCode, "Password: ", "</");
+
+        }
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
